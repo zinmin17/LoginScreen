3ba0d1871f473c578d16f98751e670c0c37419cc
zinmin17 <zm.mgmg@gmail.com>
2017-11-28 19:37:02 +0800

    office

--- src/components/Auth/Dashboard/Accounts.js
+++ src/components/Auth/Dashboard/Accounts.js
@@ -10,20 +10,55 @@ export default class Accounts extends Component {
 
   render() {
     var swipeoutBtns = [
-    {
-      text: 'Button'
-    }
+      {
+      text: 'Fund',
+      backgroundColor: '#2b638d',
+      onPress: () => { this.deleteNote(rowData) }
+     },
+      {
+        text: 'Setting',
+        backgroundColor: '#9F9B9B',
+        onPress: () => { this.duplicateNote(rowData) }
+     },
     ]
     return (
             <View style={styles.container}>
             <StatusBar barStyle = "light-content" />
 
+                <Text style={styles.titleText}> Accounts </Text>
 
-              <Swipeout right={swipeoutBtns}>
-              <View>
-                <Text>Swipe me left</Text>
-              </View>
-              </Swipeout>
+                <View style={styles.swipe_container}>
+                <View style={styles.table_head_container}>
+                        <Text style={styles.table_head}> Account </Text>
+                        <Text style={styles.table_head}> Leverage </Text>
+                        <Text style={styles.table_head}> Balance </Text>
+                </View>
+                <Swipeout right={swipeoutBtns}>
+                  <View style={styles.text_container}>
+                        <Text style={styles.account}>310045</Text>
+                        <Text style={styles.table_text}>1:200< /Text>
+                        <Text style={styles.balance}>5000.00<Text style={styles.currency}> SGD</Text>< /Text>
+                  </View>
+                </Swipeout>
+
+                <Swipeout right={swipeoutBtns}>
+                  <View style={styles.text_container}>
+                        <Text style={styles.account}>310046</Text>
+                        <Text style={styles.table_text}>1:400</Text>
+                        <Text style={styles.balance}>5400.03 <Text style={styles.currency}> JPY</Text></Text>
+                  </View>
+                </Swipeout>
+
+                <Swipeout right={swipeoutBtns}>
+                  <View style={styles.text_container}>
+                        <Text style={styles.account}>310047</Text>
+                        <Text style={styles.table_text}>1:400</Text>
+                        <Text style={styles.balance}>12493.44<Text style={styles.currency}> USD</Text></Text>
+                  </View>
+                </Swipeout>
+
+                </View>
+
             </View>
     );
   }
@@ -33,7 +68,59 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#0E3353',
-      marginTop: 50
   },
+  titleText:{
+    fontSize: 20,
+    color : '#D3D2D0',
+    textAlign: 'center',
+    marginTop: '15%',
+  },
+  swipe_container:{
+    marginTop: 50,
+  },
+  table_head_container:{
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  table_head:{
+    fontWeight: 'bold',
+    color : '#9FAFBB',
+    width: '25%',
+    margin:8,
+  },
+  text_container: {
+    height: 60,
+    borderWidth: 0.5,
+    borderColor: '#575967',
+    borderRightColor: '#F27840',
+    borderRightWidth: 3,
+    backgroundColor: '#0E3353',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+  },
+  account:{
+  fontWeight: 'bold',
+    color : '#9FAFBB',
+    width: '25%',
+    margin:8,
+  },
+  table_text:{
+    color : '#9FAFBB',
+    width: '25%',
+    margin:8,
+  },
+  balance:{
+    fontWeight: 'bold',
+    color : '#2CE39F',
+    width: '25%',
+    margin:8,
+  },
+  currency:{
+    fontWeight: 'normal',
+    color : '#DBDBDB',
+    fontSize: 10,
+    textAlignVertical: 'top',
+  }
 
 });
